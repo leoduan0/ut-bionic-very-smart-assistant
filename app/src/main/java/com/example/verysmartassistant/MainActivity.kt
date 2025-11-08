e0cadaf81a293d7c5c5db00b226a453e6ba47a68
Leo Duan <leoduan0@icloud.com>
2025-11-07 23:43:51 -0500

    initial commit

--- app/src/main/java/com/example/verysmartassistant/MainActivity.kt
+++ app/src/main/java/com/example/verysmartassistant/MainActivity.kt
@@ -1,3 +1,18 @@
+//This whole application should have only one page.
+//
+//Implement:
+//
+//* Call PSW
+//* Call mom
+//* Open apartment door
+//* Open suite door
+//
+//For *each* of the tasks above:
+//
+//* Create a function that does the specified task. Minimize the number of helper functions as the tasks should be simple enough to not need any
+//* Donate an App Action to Google Assistant that performs the task
+//* Create a section that has the text of the task (e.g. "open suite door"). Have a button that performs the action (in case manual triggering is needed)
+
 package com.example.verysmartassistant
 
 import android.os.Bundle
@@ -6,11 +21,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.tooling.preview.Preview
 import com.example.verysmartassistant.ui.theme.VerySmartAssistantTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +36,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VerySmartAssistantTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Home()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Home() {
+    Column {
+        Greeting()
+        ConnectivityButton(::connectToController)
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello! I'm your Very Smart Assistant from the University of Toronto Bioengineering Innovation and Outreach in Consulting Club (UT BIONIC)!",
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    VerySmartAssistantTheme {
-        Greeting("Android")
+fun ConnectivityButton(onClick: () -> Unit) {
+    TextButton(
+        onClick = { onClick() }
+    ) {
+        Text("Connect to Controller")
     }
+}
+
+fun connectToController() {
+    // logic
+    println("This function connects the app to the controller.")
 }
