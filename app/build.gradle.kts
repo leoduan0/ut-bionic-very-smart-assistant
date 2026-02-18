0335fc1ef2d03945efde3bf7533ccaaa16c33ba8
Milky-Way-Cookie <lianso125@gmail.com>
2026-02-17 20:42:25 -0500

    udates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.utbionic.verysmartassistant"
         minSdk = 33
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
